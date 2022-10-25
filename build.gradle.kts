333a21b1e2dbe5cc64de9939a7157ff931afa243
SirEdvin <siredvin.dark@gmail.com>
2022-10-25 16:29:34 +0300

    Fix minecraft version

--- build.gradle.kts
+++ build.gradle.kts
@@ -161,7 +161,7 @@ curseforge {
         id = CURSEFORGE_ID
         releaseType = CURSEFORGE_RELEASE_TYPE
         addGameVersion("Fabric")
-        addGameVersion("1.18.2")
+        addGameVersion("1.19.2")
         changelog = "${project.changelog.get(project.version as String).withHeader(false).toText()}"
         changelogType = "markdown"
         mainArtifact(tasks.remapJar.get().archivePath, closureOf<CurseArtifact> {
