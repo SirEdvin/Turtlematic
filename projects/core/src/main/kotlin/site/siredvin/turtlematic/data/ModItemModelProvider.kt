20919f4d48b753989001cd6abb1b36378d56b8b1
SirEdvin <me@siredvin.site>
2026-03-01 17:28:32 +0200

    feat(models): update logic for item model generation

--- projects/core/src/main/kotlin/site/siredvin/turtlematic/data/ModItemModelProvider.kt
+++ projects/core/src/main/kotlin/site/siredvin/turtlematic/data/ModItemModelProvider.kt
@@ -12,25 +12,21 @@ import site.siredvin.tweakium.modules.data.turtleUpgrades
 
 object ModItemModelProvider {
 
-    val BASE_GEAR = ResourceLocation(TurtlematicCore.MOD_ID, "item/base_gear")
-    val NETHERITE_GEAR = ResourceLocation(TurtlematicCore.MOD_ID, "item/netherite_gear")
-    val STARBOUND_GEAR = ResourceLocation(TurtlematicCore.MOD_ID, "item/starbound_gear")
-    val CREATIVE_GEAR = ResourceLocation(TurtlematicCore.MOD_ID, "item/creative_gear")
-    val FIRE = ResourceLocation(TurtlematicCore.MOD_ID, "item/fire/")
-    val BIG_FIRE = ResourceLocation(TurtlematicCore.MOD_ID, "item/big_fire/")
+    val BASE_GEAR = "base"
+    val NETHERITE_GEAR = "nether"
+    val STARBOUND_GEAR = "starbound"
+    val CREATIVE_GEAR = "creative"
 
     fun createAutomataCore(
         generators: ItemModelGenerators,
         item: Item,
-        gear: ResourceLocation,
+        gear: String,
         fire: String,
-        fireTemplate: ResourceLocation = FIRE,
     ) {
         createFlatItem(
             generators,
             item,
-            gear,
-            fireTemplate.withSuffix(fire),
+            TurtlematicCore.id("item/automata/${fire}_$gear"),
         )
     }
 
@@ -47,43 +43,43 @@ object ModItemModelProvider {
         turtleUpgrades(generators, Items.TURTLE_CHATTER.get(), baseID = ResourceLocation(TurtlematicCore.MOD_ID, "turtle/chatter"))
         turtleUpgrades(generators, Items.MIMIC_GADGET.get(), baseID = ResourceLocation(TurtlematicCore.MOD_ID, "turtle/mimic"))
 
-        createAutomataCore(generators, Items.AUTOMATA_CORE.get(), BASE_GEAR, "base")
+        createAutomataCore(generators, Items.AUTOMATA_CORE.get(), BASE_GEAR, "orange")
         createAutomataCore(generators, Items.HUSBANDRY_AUTOMATA_CORE.get(), BASE_GEAR, "green")
         createAutomataCore(generators, Items.END_AUTOMATA_CORE.get(), BASE_GEAR, "purple")
         createAutomataCore(generators, Items.PROTECTIVE_AUTOMATA_CORE.get(), BASE_GEAR, "red")
-        createAutomataCore(generators, Items.ENORMOUS_AUTOMATA_CORE.get(), CREATIVE_GEAR, "enormous", BIG_FIRE)
+        createAutomataCore(generators, Items.ENORMOUS_AUTOMATA_CORE.get(), CREATIVE_GEAR, "dark_red")
 
         createAutomataCore(generators, Items.NETHERITE_HUSBANDRY_AUTOMATA_CORE.get(), NETHERITE_GEAR, "green")
         createAutomataCore(generators, Items.NETHERITE_END_AUTOMATA_CORE.get(), NETHERITE_GEAR, "purple")
         createAutomataCore(generators, Items.NETHERITE_PROTECTIVE_AUTOMATA_CORE.get(), NETHERITE_GEAR, "red")
 
-        createAutomataCore(generators, Items.STARBOUND_HUSBANDRY_AUTOMATA_CORE.get(), STARBOUND_GEAR, "green", BIG_FIRE)
-        createAutomataCore(generators, Items.STARBOUND_END_AUTOMATA_CORE.get(), STARBOUND_GEAR, "purple", BIG_FIRE)
-        createAutomataCore(generators, Items.STARBOUND_PROTECTIVE_AUTOMATA_CORE.get(), STARBOUND_GEAR, "red", BIG_FIRE)
+        createAutomataCore(generators, Items.STARBOUND_HUSBANDRY_AUTOMATA_CORE.get(), STARBOUND_GEAR, "green")
+        createAutomataCore(generators, Items.STARBOUND_END_AUTOMATA_CORE.get(), STARBOUND_GEAR, "purple")
+        createAutomataCore(generators, Items.STARBOUND_PROTECTIVE_AUTOMATA_CORE.get(), STARBOUND_GEAR, "red")
 
-        createAutomataCore(generators, Items.CREATIVE_HUSBANDRY_AUTOMATA_CORE.get(), CREATIVE_GEAR, "green", BIG_FIRE)
-        createAutomataCore(generators, Items.CREATIVE_END_AUTOMATA_CORE.get(), CREATIVE_GEAR, "purple", BIG_FIRE)
-        createAutomataCore(generators, Items.CREATIVE_PROTECTIVE_AUTOMATA_CORE.get(), CREATIVE_GEAR, "red", BIG_FIRE)
+        createAutomataCore(generators, Items.CREATIVE_HUSBANDRY_AUTOMATA_CORE.get(), CREATIVE_GEAR, "green")
+        createAutomataCore(generators, Items.CREATIVE_END_AUTOMATA_CORE.get(), CREATIVE_GEAR, "purple")
+        createAutomataCore(generators, Items.CREATIVE_PROTECTIVE_AUTOMATA_CORE.get(), CREATIVE_GEAR, "red")
 
         createAutomataCore(generators, Items.ENCHANTING_AUTOMATA_CORE.get(), NETHERITE_GEAR, "obsidian")
         createAutomataCore(generators, Items.BREWING_AUTOMATA_CORE.get(), NETHERITE_GEAR, "light_blue")
         createAutomataCore(generators, Items.MASON_AUTOMATA_CORE.get(), NETHERITE_GEAR, "gray")
-        createAutomataCore(generators, Items.SMITHING_AUTOMATA_CORE.get(), NETHERITE_GEAR, "base")
+        createAutomataCore(generators, Items.SMITHING_AUTOMATA_CORE.get(), NETHERITE_GEAR, "orange")
         createAutomataCore(generators, Items.MERCANTILE_AUTOMATA_CORE.get(), NETHERITE_GEAR, "brown")
 
-        createAutomataCore(generators, Items.STARBOUND_ENCHANTING_AUTOMATA_CORE.get(), STARBOUND_GEAR, "obsidian", BIG_FIRE)
-        createAutomataCore(generators, Items.STARBOUND_BREWING_AUTOMATA_CORE.get(), STARBOUND_GEAR, "light_blue", BIG_FIRE)
-        createAutomataCore(generators, Items.STARBOUND_MASON_AUTOMATA_CORE.get(), STARBOUND_GEAR, "gray", BIG_FIRE)
-        createAutomataCore(generators, Items.STARBOUND_SMITHING_AUTOMATA_CORE.get(), STARBOUND_GEAR, "base", BIG_FIRE)
-        createAutomataCore(generators, Items.STARBOUND_MERCANTILE_AUTOMATA_CORE.get(), STARBOUND_GEAR, "brown", BIG_FIRE)
+        createAutomataCore(generators, Items.STARBOUND_ENCHANTING_AUTOMATA_CORE.get(), STARBOUND_GEAR, "obsidian")
+        createAutomataCore(generators, Items.STARBOUND_BREWING_AUTOMATA_CORE.get(), STARBOUND_GEAR, "light_blue")
+        createAutomataCore(generators, Items.STARBOUND_MASON_AUTOMATA_CORE.get(), STARBOUND_GEAR, "gray")
+        createAutomataCore(generators, Items.STARBOUND_SMITHING_AUTOMATA_CORE.get(), STARBOUND_GEAR, "orange")
+        createAutomataCore(generators, Items.STARBOUND_MERCANTILE_AUTOMATA_CORE.get(), STARBOUND_GEAR, "brown")
 
-        createAutomataCore(generators, Items.CREATIVE_ENCHANTING_AUTOMATA_CORE.get(), CREATIVE_GEAR, "obsidian", BIG_FIRE)
-        createAutomataCore(generators, Items.CREATIVE_BREWING_AUTOMATA_CORE.get(), CREATIVE_GEAR, "light_blue", BIG_FIRE)
-        createAutomataCore(generators, Items.CREATIVE_MASON_AUTOMATA_CORE.get(), CREATIVE_GEAR, "gray", BIG_FIRE)
-        createAutomataCore(generators, Items.CREATIVE_SMITHING_AUTOMATA_CORE.get(), CREATIVE_GEAR, "base", BIG_FIRE)
-        createAutomataCore(generators, Items.CREATIVE_MERCANTILE_AUTOMATA_CORE.get(), CREATIVE_GEAR, "brown", BIG_FIRE)
+        createAutomataCore(generators, Items.CREATIVE_ENCHANTING_AUTOMATA_CORE.get(), CREATIVE_GEAR, "obsidian")
+        createAutomataCore(generators, Items.CREATIVE_BREWING_AUTOMATA_CORE.get(), CREATIVE_GEAR, "light_blue")
+        createAutomataCore(generators, Items.CREATIVE_MASON_AUTOMATA_CORE.get(), CREATIVE_GEAR, "gray")
+        createAutomataCore(generators, Items.CREATIVE_SMITHING_AUTOMATA_CORE.get(), CREATIVE_GEAR, "orange")
+        createAutomataCore(generators, Items.CREATIVE_MERCANTILE_AUTOMATA_CORE.get(), CREATIVE_GEAR, "brown")
 
-        createFlatItem(generators, Items.FORGED_AUTOMATA_CORE.get(), NETHERITE_GEAR)
+        createFlatItem(generators, Items.FORGED_AUTOMATA_CORE.get(), TurtlematicCore.id("item/netherite_gear"))
         createFlatItem(generators, Items.FILLED_SOUL_VIAL.get(), ResourceLocation(TurtlematicCore.MOD_ID, "item/soul_vial/full"))
 
         for (i in 1..3) {
